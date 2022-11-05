245edc283c9afe4e8fcb248754f30ea003311b17
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2022-11-05 08:01:57 -0300

    Função SOMAR criada. Usei concatenação na variavel resultado no Console>WriteLine final.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 namespace Calculadora
 {
-    class Calculadora
+    class Program
     {
         public static void Main(string[] args)
         {
+            Somar();
 
         }
+        static void Somar()
+        {
+            Console.Clear();
+            Console.WriteLine("Informe o Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 + v2;
+            Console.WriteLine("O Resultado da Soma é: " + resultado);
+        }
+
     }
 }
