171e00516243eb921157dede6d7d7d5bfaaad1f8
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2022-11-05 08:14:15 -0300

    Funcao SUBTRAÇÂO criada. Nessa função usei a interpolação de String com o ${} na variavel resultado.

--- Program.cs
+++ Program.cs
@@ -5,12 +5,13 @@ namespace Calculadora
     {
         public static void Main(string[] args)
         {
-            Somar();
+            Subtracao();
 
         }
         static void Somar()
         {
             Console.Clear();
+
             Console.WriteLine("Informe o Primeiro Valor: ");
             float v1 = float.Parse(Console.ReadLine());
 
@@ -18,8 +19,22 @@ namespace Calculadora
             float v2 = float.Parse(Console.ReadLine());
 
             float resultado = v1 + v2;
-            Console.WriteLine("O Resultado da Soma é: " + resultado);
+            Console.WriteLine("O Resultado da SOMA é: " + resultado);
         }
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Informe o Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O valor da SUBTRAÇÂO é: {resultado}");
+        }
+
 
     }
 }
