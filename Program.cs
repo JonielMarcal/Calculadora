acd96e78708811b0d3a156b6196823eaf8cc47f0
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2022-11-05 08:37:18 -0300

    Funçao MULTIPLICAÇÂO criada. Interpolação, realizando a Multiplicação dentro das chaves, sem declarar variavel resultado.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Calculadora
     {
         public static void Main(string[] args)
         {
-            Subtracao();
+            Multiplicacao();
 
         }
         static void Somar()
@@ -19,7 +19,7 @@ namespace Calculadora
             float v2 = float.Parse(Console.ReadLine());
 
             float resultado = v1 + v2;
-            Console.WriteLine("O Resultado da SOMA é: " + resultado);
+            Console.WriteLine("O Resultado da SOMA é: " + resultado); //Concatenação
         }
         static void Subtracao()
         {
@@ -32,7 +32,21 @@ namespace Calculadora
             float v2 = float.Parse(Console.ReadLine());
 
             float resultado = v1 - v2;
-            Console.WriteLine($"O valor da SUBTRAÇÂO é: {resultado}");
+            Console.WriteLine($"O valor da SUBTRAÇÂO é: {resultado}"); //Interpolação
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Informe o Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O resultado da MULTIPLICAÇÂO é: {v1 * v2}");
+            // Interpolação, realizando a Multiplicação dentro das chaves, sem declarar variavel.
+
         }
 
 
