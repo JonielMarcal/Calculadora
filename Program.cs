6c876ede4d065a66477832114e98053acdae7c7b
Joniel Marçal de Souza <marcaldesouzajoniel@gmail.com>
2022-11-07 10:22:46 -0300

    Concluido Menu usando SWITCH/CASE

--- Program.cs
+++ Program.cs
@@ -16,13 +16,23 @@ namespace Calculadora
             Console.WriteLine("2 - Subtrair");
             Console.WriteLine("3 - Multiplicar");
             Console.WriteLine("4 - Dividir");
+            Console.WriteLine("5 - Sair");
 
             Console.WriteLine("------------------------------");
 
             Console.WriteLine("Selecionae a Opção Desejada");
             short res = short.Parse(Console.ReadLine());
 
+            switch (res)
+            {
+                case 1: Somar(); break;
+                case 2: Subtrair(); break;
+                case 3: Multiplicar(); break;
+                case 4: Dividir(); break;
+                case 5: System.Environment.Exit(0); break;
+                default: Menu(); break;
 
+            }
 
 
 
@@ -39,8 +49,10 @@ namespace Calculadora
 
             float resultado = v1 + v2;
             Console.WriteLine("O Resultado da SOMA é: " + resultado); //Concatenação
+            Console.ReadKey();
+            Menu();
         }
-        static void Subtracao()
+        static void Subtrair()
         {
             Console.Clear();
 
@@ -52,8 +64,10 @@ namespace Calculadora
 
             float resultado = v1 - v2;
             Console.WriteLine($"O valor da SUBTRAÇÂO é: {resultado}"); //Interpolação
+            Console.ReadKey();
+            Menu();
         }
-        static void Multiplicacao()
+        static void Multiplicar()
         {
             Console.Clear();
 
@@ -64,10 +78,12 @@ namespace Calculadora
             float v2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine($"O resultado da MULTIPLICAÇÂO é: {v1 * v2}");
+            Console.ReadKey();
+            Menu();
 
 
         }
-        static void Divisao()
+        static void Dividir()
         {
             Console.Clear();
 
@@ -78,6 +94,8 @@ namespace Calculadora
             float v2 = float.Parse(Console.ReadLine());
 
             Console.WriteLine($"O Resultado da Divisao é: {v1 / v2} ");
+            Console.ReadKey();
+            Menu();
 
 
         }
